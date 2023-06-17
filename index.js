a8338424c8c3ea9858896d6d50d7b4e5918fb3f6
peterkomo <mwangipeterkomo@gmail.com>
2023-06-17 08:08:45 +0300

    to be complited

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution here!
+const cats=["Milo", "Otis", "Garfield"]
+beforeEach(function () {
+    cats.length = 0;
+
+    cats.push('Milo', 'Otis', 'Garfield');
+    return cats
+  });
+
+function destructivelyAppendCat(Ralph){
+  cats.push(Ralph)
+}
+function destructivelyPrependCat(Bob){
+    cats.unshift(Bob)
+}
+function destructivelyRemoveLastCat(){
+     cats.pop(-1)
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift(1)
+}
+let newcat=[Broom,...cats]
+(function appendCat(Broom){
+    return cats
+}
+)
--- test/indexTest.js
+++ test/indexTest.js
@@ -48,7 +48,7 @@ describe('index.js', function () {
 
     describe('appendCat(name)', function () {
       it('appends a cat to the cats array and returns a new array, leaving the cats array unchanged', function () {
-        expect(appendCat("Broom")).to.have.ordered.members(["Milo", "Otis", "Garfield", "Broom"]);
+        expect(appendCat("broom") ) .to.have.ordered.members(["Milo", "Otis", "Garfield", "Broom"]);
 
         expect(cats).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
       });
